ddf207a3e21816d929a08ea2574dac47170234c8
Ashwin Karthik Tamilselvan <at3103@columbia.edu>
2017-09-03 23:15:18 -0700

    Added third version

--- python/rna-transcription/rna_transcription_v2.py
+++ python/rna-transcription/rna_transcription_v2.py
@@ -1,14 +1,8 @@
 from collections import defaultdict
 
 def to_rna(dna):
-	"""Converts a given dna strand into a rna strand"""
-	
-	# Create a map of dna to rna
 	dna_complement = defaultdict(str,{'G':'C','C':'G','T':'A','A':'U'})
-	
-	# Check if it is an invalid dna strand
-	if set(dna) - set(dna_complement.keys()):
-		return ''
-	
 	rna = map(dna_complement.get, list(dna))
+	if None in rna:
+		return ''
 	return ''.join(rna)
