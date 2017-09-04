80cf63b5374da17a784b6410ecbe1aab7d1cbea6
Ashwin Karthik Tamilselvan <at3103@columbia.edu>
2017-09-03 23:09:17 -0700

    Added second version

--- python/rna-transcription/rna_transcription_v1.py
+++ python/rna-transcription/rna_transcription_v1.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 def to_rna(dna):
 	dna_complement = defaultdict(str,{'G':'C','C':'G','T':'A','A':'U'})
 	rna = map(dna_complement.get, list(dna))
-	if None in rna:
+	try:
+		return ''.join(rna)
+	except:
 		return ''
-	return ''.join(rna)
