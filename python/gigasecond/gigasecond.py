b48b4a49d0bdfed72820da83b4f2e57855c21a4d
Ashwin Karthik Tamilselvan <at3103@columbia.edu>
2017-09-05 19:07:30 -0700

    Solved gigaseconds

---
+++ python/gigasecond/gigasecond.py
@@ -0,0 +1,5 @@
+from datetime import timedelta
+
+def add_gigasecond(birth_date):
+	delta = timedelta(seconds=10**9)
+	return birth_date + delta    
--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,7 +1,12 @@
 import re
 from collections import Counter 
 
+def update_count(wrd_dict, word):
+	# Ignore empty char
+	if word:
+		wrd_dict[word] += 1
+	return wrd_dict
+
 def word_count(sentence):
-	word_count =  Counter(re.split(r'[^a-zA-Z0-9]',sentence.lower()))
-	del word_count['']
-	return word_count
+	word_list = re.split(r'[^a-zA-Z0-9]',sentence.lower())
+	return reduce(update_count, word_list, Counter())
